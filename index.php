5b2fd19498c2cbdeb7a6d407b3ef6c6fe34cb42d
Ayush Katoch <ayush98katoch@gmail.com>
2024-05-04 11:39:48 +0530

    hm

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 header('Content-Type: application/json');
 
 
-print_r($_POST);
 // if request is post
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+    
+    print_r($_POST);
     $arr = json_encode($_POST);
 
     $file = file_get_contents('data.txt');
@@ -23,16 +23,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // close file
     fclose($file);
     // print success message
-    echo 'Data saved to data.txt';
+    print_r(json_encode(array('message' => 'Data saved successfully', 'data' => $arr)));
+
+    exit(0);
 }
 
 
 // read from data.txt and print it 
 $file = file_get_contents('data.txt');
 $file = json_decode($file, true);
-print("<pre>");
+
 print_r($file);
-print("</pre>");
 
 
 
